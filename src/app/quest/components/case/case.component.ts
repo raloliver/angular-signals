3a99cc695fb9f2612b2f7843c155e356e7d615bd
raloliver <raloliver.ws@gmail.com>
2026-08-02 14:51:22 +0100

    feat: add styles to quest components

--- src/app/quest/components/case/case.component.ts
+++ src/app/quest/components/case/case.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ChoiceComponent } from '../choice/choice.component';
 
 @Component({
   selector: 'case',
   standalone: true,
+  imports: [ChoiceComponent],
   templateUrl: './case.component.html',
   styleUrl: './case.component.css',
 })
