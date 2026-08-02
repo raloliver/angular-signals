d7ff8d557945f80b26cb486fea6d28e4a4c057e3
raloliver <raloliver.ws@gmail.com>
2026-08-02 14:38:21 +0100

    feat: create case and choice component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { QuestComponent } from './quest/quest.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [QuestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/quest/components/case/case.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'case',
+  standalone: true,
+  templateUrl: './case.component.html',
+  styleUrl: './case.component.css',
+})
+export class CaseComponent {}
