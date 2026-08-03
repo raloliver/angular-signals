2f9eaf39c617306f4a8aa4f017a0e366063c7c3c
raloliver <raloliver.ws@gmail.com>
2026-08-03 17:41:03 +0100

    feat: get data from API

--- src/app/quest/interfaces/case.interface.ts
+++ src/app/quest/interfaces/case.interface.ts
@@ -3,3 +3,12 @@ export interface CaseInterface {
   choice: string;
   options: string[];
 }
+
+export interface TriviaOptions {
+  type: string;
+  difficulty: string;
+  category: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+}
